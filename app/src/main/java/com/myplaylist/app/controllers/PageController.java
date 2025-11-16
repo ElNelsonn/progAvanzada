ac8006c7364e47b178c9d36ee57cb91d7a31ca33
ElNelsonn <santbord@gmail.com>
2025-11-16 14:41:52 -0300

    FIN

--- app/src/main/java/com/myplaylist/app/controllers/PageController.java
+++ app/src/main/java/com/myplaylist/app/controllers/PageController.java
@@ -4,9 +4,7 @@ package com.myplaylist.app.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
